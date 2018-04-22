c8c47511fa9e7ec1ab8bfdcb0856b3c19a0fd86c
shenwenley <shenwenley@yahoo.com>
2018-04-22 13:30:33 -0400

    first commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float moveSpeed;
+
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update () {
+        Vector2 moveDir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        if(moveDir.magnitude > 2)
+        {
+            moveDir = moveDir.normalized;
+        }
+        transform.Translate(moveDir * moveSpeed * Time.deltaTime);
+	}
+}
