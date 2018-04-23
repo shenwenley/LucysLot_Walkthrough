29f5ea0233b1100e7a160ebea3d09db22d0209cc
shenwenley <shenwenley@yahoo.com>
2018-04-22 22:54:07 -0400

    Tooltips can be made based on selected object and Tooltip Manager. However, they don't wrap horizontally which is a problem.

---
+++ Assets/Scripts/TooltipText.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TooltipText : MonoBehaviour {
+
+    public float maxTime = 3f;
+
+    private float timer;
+
+    private GameObject sizeText;
+
+    private void Start()
+    {
+        sizeText = transform.GetChild(0).gameObject;
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer >= maxTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetText(string message)
+    {
+        if(sizeText == null)
+        {
+            sizeText = transform.GetChild(0).gameObject;
+        }
+        sizeText.GetComponent<Text>().text = message;
+        sizeText.transform.GetChild(1).GetComponent<Text>().text = message;
+    }
+
+}
