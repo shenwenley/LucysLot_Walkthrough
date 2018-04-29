55bdf297b1118bce135a07afc2d8b70e75f5f38d
Shafi Adam <adam.shafi94@gmail.com>
2018-04-29 15:30:34 -0400

    lockedDoor sounds

--- Assets/Scripts/LockedDoor.cs
+++ Assets/Scripts/LockedDoor.cs
@@ -1,9 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class SelectObjectNoMessage : MonoBehaviour {
+public class LockedDoor : MonoBehaviour {
 
     public AudioClip openDoor;
     private AudioSource source;
@@ -15,32 +14,39 @@ public class SelectObjectNoMessage : MonoBehaviour {
     public string levelName;
     public Vector2 loc;
 
-    private void Start() {
+    private void Start()
+    {
         touching = false;
         renderer = GetComponent<SpriteRenderer>();
         startColor = renderer.material.color;
         source = GetComponent<AudioSource>();
     }
 
-    private void Update() {
-        if(Input.GetMouseButtonDown(0) && touching) {
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && touching)
+        {
             StaticPosition.location = loc;
             source.PlayOneShot(openDoor, 1);
-            SceneManager.LoadScene(levelName);
+            //SceneManager.LoadScene(levelName);
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision) {
-        if(collision.gameObject.CompareTag("Player")) {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
             Debug.Log("enter player");
             renderer.material.color = Color.red;
             touching = true;
         }
     }
 
-    private void OnTriggerExit2D(Collider2D collision) {
+    private void OnTriggerExit2D(Collider2D collision)
+    {
         Debug.Log("exit");
-        if(collision.gameObject.CompareTag("Player")) {
+        if (collision.gameObject.CompareTag("Player"))
+        {
             Debug.Log("exit player");
             renderer.material.color = startColor;
             touching = false;
